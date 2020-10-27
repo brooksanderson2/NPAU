3f758099237abbd56add936cc55be22f95f38380
brooksanderson2 <brooksanderson2@mail.weber.edu>
2020-10-27 10:44:56 -0600

    Init articles branch

---
+++ NoPoorAfrica.Models/Models/Articles/article_generic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NoPoorAfrica.Models.Articles
+{
+    public class Article_Generic
+    {
+        [Key]
+        public int ArticleId { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+
+    }
+}
