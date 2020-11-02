0305a78a1807d5d3577d1be84d59fc0fa1f4871f
devyn <devynwoodruff@mail.weber.edu>
2020-11-02 16:29:51 -0700

    Added initial donation payment stuff.

--- NoPoorAfrica/Pages/User/DonationCause/DonationPayment.cshtml.cs
+++ NoPoorAfrica/Pages/User/DonationCause/DonationPayment.cshtml.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NoPoorAfrica.DataAccess.Data.Repository.IRepository;
 
 namespace NoPoorAfrica.Pages.User.DonationCause
 {
     public class DonationPaymentModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DonationPaymentModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public void OnGet()
         {
         }
