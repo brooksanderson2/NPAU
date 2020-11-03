41ce394e780c9ce005a0d6b4b282ec73f67979b2
Brooks Anderson <65507102+brooksanderson2@users.noreply.github.com>
2020-11-02 23:50:03 -0700

    First iteration of main articles page with detailed view of each article.

---
+++ NoPoorAfrica.Models/ViewModels/ArticlesVM.cs
@@ -0,0 +1,10 @@
+﻿using NoPoorAfrica.Models.Models;
+using System.Collections.Generic;
+
+namespace NoPoorAfrica.Models.ViewModels
+{
+    public class ArticlesVM
+    {
+        public IEnumerable<Article> Articles { get; set; }
+    }
+}
--- NoPoorAfrica/Pages/Articles/Index.cshtml.cs
+++ NoPoorAfrica/Pages/Articles/Index.cshtml.cs
@@ -4,13 +4,47 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NoPoorAfrica.DataAccess.Data.Repository.IRepository;
+using NoPoorAfrica.Models.Models;
+using NoPoorAfrica.Models.ViewModels;
 
 namespace NoPoorAfrica.Pages.Articles
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        const int PAGESIZE = 3;
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ArticlesVM ArticlesList { get; set; }
+        public Article DetailedArticle { get; set; }
+        public bool DetailedView { get; set; }
+
+        public IndexModel(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void OnGet(string id)
+        {
+            DetailedView = false;
+            ArticlesList = new ArticlesVM();
+
+            if (id == null) 
+            {
+                //If no article is selected in url, show recent articles
+
+                ArticlesList.Articles = _unitOfWork.Article.GetAll();
+            }
+            else
+            {
+                //Show article specified in url {id}
+                //Articles?id=articleRoute
+
+                DetailedView = true;
+
+                DetailedArticle = _unitOfWork.Article.GetFirstOrDefault(i => i.RouteName == id);
+            }
         }
     }
 }
