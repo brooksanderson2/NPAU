851d7df84d50081504cf5d193567fba4d4286685
brooksanderson2 <brooksanderson2@mail.weber.edu>
2020-11-10 10:17:16 -0700

    Article models revamp (pre-migration).

--- NoPoorAfrica.Models/Models/Article.cs
+++ NoPoorAfrica.Models/Models/Article.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
@@ -15,12 +16,13 @@ namespace NoPoorAfrica.Models.Models
         /// Title of the article.
         /// </summary>
         [Display(Name = "Title")]
+        [Required]
         public string Title { get; set; }
 
         /// <summary>
         /// Font family for the title.
         /// </summary>
-        [Display(Name = "")]
+        [Display(Name = "Title Font")]
         public string TitleFont { get; set; } = "Arial"; // TODO: What is going to be the default font family?
                                                          //       Maybe the template sets the default? -- Brooks
 
@@ -50,22 +52,31 @@ namespace NoPoorAfrica.Models.Models
         /// May be wise to make this a foreign key to a template model - may be unnecessary. 
         /// </summary>
         [Display(Name = "Article Template")]
+        [Required]
         public string Template { get; set; } = "template_1";
-
-        /// <summary>
-        /// Images to head the article. If there is more than one, becomes a gallery.
-        /// </summary>
-        IEnumerable<string> Images { get; set; }
 
         /// <summary>
         /// Date of article
         /// </summary>
+        [Required]
         [Display(Name = "Date")]
-        public DateTime Date { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        /// <summary>
+        /// Date of most recent update to the article. Will show as PublishDate if never updated.
+        /// </summary>
+        public DateTime UpdateDate { get; set; }
 
         /// <summary>
         /// Used for routing inside the Article page controller, since the page is dynamic.
         /// </summary>
+        [Required]
         public string RouteName { get; set; }
+
+        /// <summary>
+        /// Foreign key to ArticleCategory
+        /// </summary>
+        [ForeignKey("ArticleCategory")]
+        public virtual ArticleCategory ArticleCategory { get; set; }
     }
 }
---
+++ NoPoorAfrica.Models/Models/ArticleFiles.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NoPoorAfrica.Models.Models
+{
+    public class ArticleFiles
+    {
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Foreign key to Article
+        /// </summary>
+        [Required]
+        [ForeignKey("ArticleId")]
+        public virtual int ArticleId { get; set; }
+
+        /// <summary>
+        /// Path to file
+        /// </summary>
+        [Required]
+        public string File { get; set; }
+    }
+}
