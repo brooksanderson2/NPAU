5b3ed71d5802c5f835e8bffbba4c41848c2a7a28
devyn <devynwoodruff@mail.weber.edu>
2020-11-27 16:06:17 -0700

    Added functionality to delete pending wire and add new donation details after approval of pending wire.

--- NoPoorAfrica/Controllers/PendingWiresController.cs
+++ NoPoorAfrica/Controllers/PendingWiresController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using NoPoorAfrica.DataAccess.Data.Repository.IRepository;
+using NoPoorAfrica.Models.Models;
 
 namespace NoPoorAfrica.Controllers
 {
@@ -26,5 +27,68 @@ namespace NoPoorAfrica.Controllers
         {
             return Json(new { data = _unitOfWork.PendingWire.GetAll(null, null, "DonationCause") });
         }
+
+
+
+
+        [HttpPost("{id}")]
+        public IActionResult Post(int id)
+        {
+            try
+            {
+
+                DonationDetails obj = new DonationDetails();
+                var objFromDb = _unitOfWork.PendingWire.GetFirstOrDefault(u => u.Id == id);
+                if (objFromDb == null)
+                {
+                    return Json(new { success = false, message = "Error while approving" });
+                }
+                obj.UserId = objFromDb.UserId;
+                obj.DonationCauseId = objFromDb.DonationCauseId;
+                obj.DonorName = objFromDb.DonorName;
+                obj.DonationTotal = objFromDb.DonationTotal;
+                obj.PaymentStatus = objFromDb.PaymentStatus;
+                obj.Email = objFromDb.Email;
+                obj.Comments = objFromDb.Comments;
+                obj.FollowUp = objFromDb.FollowUp;
+                obj.TransactionId = objFromDb.TransactionId;
+                obj.DonationDate = objFromDb.DonationDate;
+                obj.DonationCause = objFromDb.DonationCause;
+
+                _unitOfWork.PendingWire.Remove(objFromDb);
+                _unitOfWork.DonationDetails.Add(obj);
+                _unitOfWork.Save();
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Error while approving" });
+            }
+            return Json(new { success = true, message = "Approval Successful" }); 
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+            try
+            {
+
+                var objFromDb = _unitOfWork.PendingWire.GetFirstOrDefault(u => u.Id == id);
+                if (objFromDb == null)
+                {
+                    return Json(new { success = false, message = "Error while deleting" });
+                }
+
+                _unitOfWork.PendingWire.Remove(objFromDb);
+                _unitOfWork.Save();
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+
     }
 }
--- NoPoorAfrica/wwwroot/js/pendingWires.js
+++ NoPoorAfrica/wwwroot/js/pendingWires.js
@@ -21,20 +21,34 @@ function loadList() {
 					return type === 'sort' ? data : moment(data).format('MM/DD/YYYY');
 				}, "width": "15%"
 			},
-			{ "data": "email", "width": "15%" },
+			{ "data": "email", "width": "10%" },
 			{ "data": "donationCause.title", "width": "15%" },
-			{ "data": "comments", "width": "15%" },
-			{ "data": "followUp", "width": "15%" },
+			{ "data": "comments", "width": "10%" },
+			{ "data": "followUp", "width": "10%" },
+			{
+				"data": "id",
+				"render": function (data) {
+					return `
+						<div class="text-center">
+							<a onClick=Approve('/api/PendingWires/'+${data})
+							class="btn btn-success text-white style="cursor:pointer; width:100px;">
+							<i class="far fa-edit"></i>
+							Approve
+							</a>
+							<a onClick=Delete('/api/PendingWires/'+${data})
+							class="btn btn-danger text-white style="cursor:pointer; width:100px;">
+							<i class="far fa-trash-alt"></i>
+							Delete
+							</a>
+						</div>`;
+				}, width: "15%"
+			}
 		],
 		"language": {
 			"emptyTable": "no data found."
 		},
 		"width": "100%",
 		"order": [[2, "asc"]],
-		"dom": 'Blfrtip',
-		"buttons": [
-			'csv', 'pdf', 'print'
-		],
 	});
 }
 
@@ -49,6 +63,32 @@ function Delete(url) {
 		if (willDelete) {
 			$.ajax({
 				type: 'DELETE',
+				url: url,
+				success: function (data) {
+					if (data.success) {
+						toastr.success(data.message);
+						dataTable.ajax.reload();
+					}
+					else {
+						toastr.error(data.message);
+					}
+				}
+			});
+		}
+	});
+}
+
+function Approve(url) {
+	swal({
+		title: "Are you sure you want to approve?",
+		text: "You will not be able to restore the data!",
+		icon: "warning",
+		buttons: true,
+		dangerMode: true
+	}).then((willDelete) => {
+		if (willDelete) {
+			$.ajax({
+				type: 'POST',
 				url: url,
 				success: function (data) {
 					if (data.success) {
