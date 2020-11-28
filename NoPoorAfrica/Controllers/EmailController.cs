28738a3e3a07279ca501847b9a48c00a09a4c832
Stephanie Spencer <Stephanie.spencer.22@hotmail.com>
2020-11-28 16:29:17 -0700

    Emails Page

---
+++ NoPoorAfrica/Controllers/EmailController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NoPoorAfrica.DataAccess.Data.Repository.IRepository;
+using NoPoorAfrica.Utility;
+
+namespace NoPoorAfrica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        
+
+        public EmailController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+           
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+                return Json(new { data = _unitOfWork.OrderHeader.GetAll() });
+
+        }
+    }
+}
---
+++ NoPoorAfrica/wwwroot/js/email.js
@@ -0,0 +1,64 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/Email",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+
+            { "data": "email", "width": "25%" },
+            {
+                "data": "purchaseDate",
+                "render": function (data, type) {
+                    return type === 'sort' ? data : moment(data).format('MM/DD/YYYY');
+                }, width: "25%"
+            },
+            
+            { "data": "transactionID", "width": "25%" },
+            { "data": "EmailPrefernce", "width": "25%" },
+        
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%",
+        "order": [[2, "asc"]],
+        "dom": 'Blfrtip',
+        "buttons": [
+            'csv', 'pdf', 'print'
+        ],
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
