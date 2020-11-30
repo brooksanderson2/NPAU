4f2720adbea8348926ddabe6572fa3d4d57a2f58
brooksanderson2 <brooksanderson2@mail.weber.edu>
2020-11-29 21:16:15 -0700

    Moved templates into ViewComponents.

---
+++ NoPoorAfrica.Models/ViewModels/ArticlesTemplateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using NoPoorAfrica.Models.Models;
+using System.Collections.Generic;
+
+namespace NoPoorAfrica.Models.ViewModels
+{
+    public class ArticlesTemplateViewModel : Article
+    {
+        public List<ArticleFiles> ImageGallery { get; set; }
+    }
+}
--- NoPoorAfrica/wwwroot/js/adminArticleImageUpload.js
+++ NoPoorAfrica/wwwroot/js/adminArticleImageUpload.js
@@ -1,6 +1,4 @@
-﻿import { Modal } from "bootstrap";
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
 
     //getPositions();
 
