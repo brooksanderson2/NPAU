2108ea63a51352fe7286b11eca53b59759a5a875
brooksanderson2 <brooksanderson2@mail.weber.edu>
2020-11-29 19:51:30 -0700

    Preview template

--- NoPoorAfrica/wwwroot/js/adminArticleImageUpload.js
+++ NoPoorAfrica/wwwroot/js/adminArticleImageUpload.js
@@ -1,4 +1,6 @@
-﻿$(document).ready(function () {
+﻿import { Modal } from "bootstrap";
+
+$(document).ready(function () {
 
     //getPositions();
 
@@ -47,42 +49,5 @@ function deleteImage(imgPath){
         "datatype": "json"
     }).done(function (result) {
 
-    });
-}
-
-function prepareButtonUpload() {
-    $('#btnFUpload').on('click', function () {
-        var files = $('#fUpload').prop("files");
-        var articleId = $('#articleId').prop("value");
-        var images = $('images').prop("value");
-
-        var fdata = new FormData();
-        fdata.append("articleId", articleId);
-        for (var i = 0; i < images.length; i++) {
-            fdata.append("images", images[i]);
-        }
-        for (var i = 0; i < files.length; i++) {
-            fdata.append("files", files[i]);
-        }
-
-        if (files.length > 0) {
-            $.ajax({
-                type: "POST",
-                url: "?handler=Upload",
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader("XSRF-TOKEN",
-                        $('input:hidden[name="__RequestVerificationToken"]').val());
-                },
-                data: fdata,
-                contentType: false,
-                processData: false,
-                success: function (response) {
-                    alert('Image(s) uploaded successfully.')
-                }
-            });
-        }
-        else {
-            alert('Please select an image.')
-        }
     });
 }
